33454d6f60476b04b03e2d4565c2bee0d46a27ab
OlekKovalevych <kovalevich.148@gmail.com>
2022-06-29 10:19:20 +0200

    created permit params

--- app/admin/goals.rb
+++ app/admin/goals.rb
@@ -1,18 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Goal do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :content, :order
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :content, :order]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+
+  permit_params :title, :content, :order
+
 end
